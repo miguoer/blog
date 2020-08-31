b93d469933b1bc5b709ce57aaaddbbb3690c83cd
huanglin <huanglin@wtsd.cn>
2020-08-31 18:56:20 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,15 @@ const sidebar = require('./sidebar');
 module.exports = {
     title: '工欲善其事 必先利其器',
     base:"/blog/",
+
     description: 'Just playing around',
     themeConfig: {
         sidebarDepth:2,
         nav,
         sidebar,
         lastUpdated:"最后更新时间",
-        docsDir:'docs'
+        docsDir:'docs',
+        repo:"https://miguoer.github.io/blog"
       },
       extraWatchFiles:[
           "./nav.js",
